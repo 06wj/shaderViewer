295941ce4950a559d185622d5f673ca11db15ca2
06wj <06wj@163.com>
2018-05-31 17:52:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ShaderViewer
+Preprocess and format the shader code.
+
+### demo
+[http://06wj.github.io/shaderViewer/demo/](http://06wj.github.io/shaderViewer/demo/)
+
+![](https://gw.alicdn.com/tfs/TB16wlruL5TBuNjSspcXXbnGFXa-1470-1338.png)
+### License
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ src/index.js
@@ -0,0 +1,56 @@
+import jsbeautifier from 'js-beautify';
+var Prism = require('prismjs');
+var loadLanguages = require('prismjs/components/index.js');
+loadLanguages(['glsl']);
+
+import {
+    Compiler
+} from '../lib/compiler';
+
+const app = {
+    compiler(code, callback, options = {}) {
+        const compiler = new Compiler(Object.assign({
+            constants: {
+                GL_SL: '1'
+            }
+        }, options));
+
+        compiler.once('error', function(msg) {
+            callback && callback(msg, null);
+        });
+
+        compiler.once('success', function(result) {
+            callback && callback(null, result);
+        });
+
+        compiler.compile(code);
+    },
+    beautify(code){
+        return jsbeautifier(code);
+    },
+    show(){
+        const preCode = this.codeElem.value;
+        this.compiler(preCode, (error, code) => {
+            if(code){
+                code = this.beautify(code);
+                code = '\n' + code;
+                const html = Prism.highlight(code, Prism.languages.glsl, 'glsl');
+                this.viewElem.innerHTML = html;
+            }
+        });
+    },
+    init(){
+        const codeElem = this.codeElem = document.getElementById('code');
+        const viewElem = this.viewElem = document.getElementById('codeView');
+        
+        this.bindEvent();
+        this.show();
+    },
+    bindEvent(){
+        this.codeElem.oninput = () => {
+            this.show();
+        };
+    }
+};
+
+app.init();
