bd1a82b35d4039da06a58f83590ffdd857f1b436
06wj <06wj@163.com>
2018-06-08 11:40:57 +0800

    add remove unused func

--- src/compiler.js
+++ src/compiler.js
@@ -1,9 +1,16 @@
 import jsbeautifier from 'js-beautify';
+import glsl from 'glsl-man';
 import Prism from '../lib/prism.js';
 import {
     Compiler
 } from '../lib/compiler';
 
+
+/**
+ * options
+ * options.removeUnused
+ * options.ignoreConstantError
+ */
 const compiler = {
     preprocess(code, callback, options = {}) {
         const compiler = new Compiler(Object.assign({
@@ -27,6 +34,50 @@ const compiler = {
         code = jsbeautifier(code).replace(/\$([\w]+)\$/g, '#$1').replace(/\n\n+/g, '\n');
         return code;
     },
+    removeUnused(code){
+        const ast = glsl.parse(code);
+
+        const funcInfoDict = {};
+        const calledFunc = {
+            main: true
+        };
+        const function_declaration = glsl.query.all(ast, glsl.query.selector('function_declaration'));
+
+        function_declaration.forEach(node => {
+            const name = node.name;
+            const funcInfo = funcInfoDict[name] = funcInfoDict[name] || {
+                nodes: [],
+                called: []
+            };
+            funcInfo.nodes.push(node);
+
+            const called = funcInfo.called;
+            glsl.query.all(node, glsl.query.selector('function_call')).forEach(calledNode => {
+                called.push(calledNode.function_name);
+            });
+        });
+
+        funcInfoDict.main.called.forEach(name => {
+            calledFunc[name] = true;
+            const funcInfo = funcInfoDict[name];
+            if (funcInfo) {
+                funcInfo.called.forEach(name => {
+                    calledFunc[name] = true;
+                });
+            }
+        });
+
+        for (name in funcInfoDict) {
+            const info = funcInfoDict[name];
+            if (info && !calledFunc[name]) {
+                info.nodes.forEach(node => {
+                    glsl.mod.remove(node);
+                });
+            }
+        }
+
+        return glsl.string(ast);
+    },
     hightlight(code){
         code = '\n' + code;
 
@@ -39,7 +90,13 @@ const compiler = {
                     code = `//${error}\n` + preCode; 
                 }
                 else{
-                    code = this.beautify(code);
+                    if(options.removeUnused){
+                        code = this.removeUnused(code);
+                        console.log(code)
+                    }
+                    else{
+                        code = this.beautify(code);
+                    }
                 }
                 callback(error, code);                
             }, options);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import compiler from './compiler';
 
 const app = {
-    show(){
+    show() {
         const preCode = this.codeElem.value;
+        const options = {
+            ignoreConstantError:false,
+            removeUnused:true
+        };
+
         compiler.parseHighlight(preCode, (error, code) => {
             this.viewElem.innerHTML = code;
-        });
+        }, options);
     },
-    init(){
+    init() {
         const codeElem = this.codeElem = document.getElementById('code');
         const viewElem = this.viewElem = document.getElementById('codeView');
-        
+
         this.bindEvent();
         this.show();
     },
-    bindEvent(){
+    bindEvent() {
         this.codeElem.oninput = () => {
             this.show();
         };
